ab54224dbe6f66ce689f0849ab3cde7d74879c8c
Aiden Ly <atl.aidenly@gmail.com>
2025-03-03 13:32:16 -0500

    finished triangle, circle, undo, clear

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function (event) {
+    let x;
+    let y;
     let c = document.getElementById("canvasbox");
     let ctx = c.getContext("2d");
     let undoButton = document.getElementById("undo");
@@ -28,14 +30,51 @@ window.addEventListener("load", function () {
     }
 
     class Triangle {
+        constructor(x, y, colour, size) {
+            this.colour = colour;
+            this.size = size;
+            this.x = x;
+            this.y = y;
+        }
+
+        draw(ctx) {
+            ctx.fillStyle = this.colour;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x + this.size / 2, this.y - this.size);
+            ctx.lineTo(this.x - this.size / 2, this.y - this.size);
+            ctx.closePath();       
+            ctx.fill();
+        }
+        
 
     }
 
     class Circle {
+        constructor(x, y, colour, size) {
+            this.colour = colour;
+            this.size = size;
+            this.x = x;
+            this.y = y;
+        }
 
+        draw(ctx) {
+            ctx.fillStyle = this.colour;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+            ctx.closePath();   
+            ctx.fill();
+        }
     }
 
     let currentObject = new Square(0, 0, colourBox.value, sizeBox.value);
+
+    function loadShapes() {
+        ctx.clearRect(0, 0, c.width, c.height);
+        for (object of onScreen) {
+            object.draw(ctx);
+        }
+    }
 
     sizeBox.addEventListener("input", function (event) {
         if (this.value <= 0) {
@@ -49,17 +88,41 @@ window.addEventListener("load", function () {
         currentObject.colour = this.value;
     });
 
-    function loadShapes() {
-        for (object of onScreen) {
-            object.draw(ctx);
+    undoButton.addEventListener("click", function (event) {
+        if (onScreen.length > 0) {
+            onScreen.pop();
+            loadShapes();
         }
-    }
+
+    });
+
+    clearButton.addEventListener("click", function (event) {
+        onScreen = [];
+        loadShapes();
+    });
+
+    square.addEventListener("click", function (event) {
+        currentObject = new Square(0, 0, colourBox.value, sizeBox.value);
+        loadShapes();
+    });
+
+    triangle.addEventListener("click", function (event) {
+        currentObject = new Triangle(0, 0, colourBox.value, sizeBox.value);
+        loadShapes();
+    });
+
+    circle.addEventListener("click", function (event) {
+        currentObject = new Circle(0, 0, colourBox.value, sizeBox.value);
+        loadShapes();
+    });
+
+
+
+
 
     c.addEventListener("mousemove", function (event) {
-        let x = event.pageX - this.offsetLeft;
-        let y = event.pageY - this.offsetTop;
-
-        ctx.clearRect(0, 0, c.width, c.height);
+        x = event.pageX - this.offsetLeft;
+        y = event.pageY - this.offsetTop;
 
         loadShapes();
 
@@ -70,22 +133,16 @@ window.addEventListener("load", function () {
     });
 
     c.addEventListener("mousedown", function (event) {
-
-        let x = event.pageX - this.offsetLeft;
-        let y = event.pageY - this.offsetTop;
-
-        let newObject = new Square(x, y, currentObject.colour, currentObject.size);
+        let newObject;
+        if (currentObject instanceof Square) {
+            newObject = new Square(x, y, currentObject.colour, currentObject.size);
+        } else if (currentObject instanceof Triangle) {
+            newObject = new Triangle(x, y, currentObject.colour, currentObject.size);
+        } else if (currentObject instanceof Circle) {
+            newObject = new Circle(x, y, currentObject.colour, currentObject.size);
+        }
         onScreen.push(newObject);
         newObject.draw(ctx);
-
-
     });
-
-
-
-
-
-
-
 
 });
